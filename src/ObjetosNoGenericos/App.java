17c87de7b7897ff4eae7874520678e894fa77c7a
agustina_heredia <cuadernomenstrual@gmail.com>
2023-01-22 12:58:46 +0100

    first commit

---
+++ src/ObjetosNoGenericos/App.java
@@ -0,0 +1,12 @@
+package ObjetosNoGenericos;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		NoGenericMethods constructor1 = new NoGenericMethods("Maria", "Ana","Pedro");
+
+		NoGenericMethods constructor2 = new NoGenericMethods("Ana", "Pedro","Maria");
+	}
+
+}
